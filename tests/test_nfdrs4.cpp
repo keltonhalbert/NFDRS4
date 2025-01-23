7866c2723e73fed2e85cde4187b836a90d2046d6
Kelton Halbert <kelton.halbert@noaa.gov>
2025-01-23 15:15:34 -0600

    Added the ability to compile and run tests, and so far have identified an inability to instantiate an object because of missing cpp code implementations

---
+++ tests/test_nfdrs4.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <nfdrs4.h>
+
+#include "doctest.h"
+
+TEST_CASE("") {
+    auto obj = NFDRS4();
+    // just trying to get this to all work
+    CHECK(1 == 1);
+}
